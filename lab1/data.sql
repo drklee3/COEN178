6847400aecc5fa0603854396af713e3bc3a814e9
drklee3 <dlee3@scu.edu>
2019-01-14 18:37:45 -0800

    Add query and spool files

---
+++ lab1/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO test VALUES (10, 'ten');
+INSERT INTO test VALUES (11, 'eleven');
+INSERT INTO test VALUES (12, 'twelve');
+INSERT INTO test VALUES (13, 'thirteen');
+INSERT INTO test VALUES (14, 'fourteen');
---
+++ lab1/init.sql
@@ -0,0 +1,8 @@
+CREATE table Staff_2010 (
+  Last VARCHAR(20),
+  First VARCHAR(20),
+  status VARCHAR(20),
+  salary NUMBER(10,2),
+  salary_type VARCHAR(40),
+  title VARCHAR(200)
+);
