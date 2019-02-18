79cbfba30ea8196aeb96266d1227a928bfd8e32e
drklee3 <dlee3@scu.edu>
2019-02-18 15:40:32 -0800

    Finish assigment 2

--- assignment2/a2_queries_dlee.sql
+++ assignment2/a2_queries_dlee.sql
@@ -1,60 +1,63 @@
 -- 1
   SELECT name
          , price
-    FROM MealItem
+    FROM MealItem2
 ORDER BY price;
 
 -- 2
   SELECT name
          , price
          , calories
-    FROM MealItem
+    FROM MealItem2
 ORDER BY price, calories;
 
 -- 3
 SELECT name
-  FROM MealItem
+  FROM MealItem2
  WHERE calories = 
        (SELECT MAX(calories)
-          FROM MealItem);
+          FROM MealItem2);
 
 -- 4
-SELECT name
-  FROM MealItem
- WHERE itemId =
-       (SELECT name
-          FROM
-                (SELECT name
-                        , COUNT(*) AS occurences
-                   FROM Order_Item
-               GROUP BY itemId
-               ORDER BY occurences DESC
-                  LIMIT 1));
+  SELECT name
+    FROM MealItem2
+   WHERE name = (SELECT name
+                   FROM (SELECT name
+                                , COUNT(*) AS order_count2
+                           FROM MealItem2
+                                NATURAL JOIN OrderItem
+                       GROUP BY name
+                       ORDER BY COUNT(*) DESC)
+                   WHERE ROWNUM = 1);
+   
+
 
 -- 5
 SELECT name
        , price
-  FROM MealItem
+  FROM MealItem2
  WHERE itemId NOT IN
        (SELECT itemId
-          FROM Order_Item);
+          FROM OrderItem);
 
 -- 6
-  SELECT phone
-         , COUNT(*)
-    FROM MealOrder
-GROUP BY phone ??
+SELECT phone, COUNT(*)
+  FROM MealOrder2
+GROUP BY phone
+HAVING COUNT(*) = (SELECT MAX(COUNT(*))
+                     FROM MealOrder2
+                 GROUP BY phone);
 
 -- 7
   SELECT orderId
          , SUM(calories)
-    FROM Order_Item
-         NATURAL JOIN MealItem
+    FROM OrderItem
+         NATURAL JOIN MealItem2
 GROUP BY orderId;
 
 -- 8
-UPDATE MealItem
+UPDATE MealItem2
    SET price = 0.9 * price
  WHERE itemId NOT IN 
        (SELECT itemId
-          FROM Order_Item);
+          FROM OrderItem);
--- assignment2/a2_tables_dlee.sql
+++ assignment2/a2_tables_dlee.sql
@@ -1,23 +1,23 @@
-CREATE TABLE MealItem (
+CREATE TABLE MealItem2 (
     itemId   VARCHAR(20) PRIMARY KEY,
     name     VARCHAR(20),
     price    NUMBER(2),
     calories INTEGER
 );
 
-CREATE TABLE MealOrder (
+CREATE TABLE MealOrder2 (
     orderId VARCHAR(20) PRIMARY KEY,
     name    VARCHAR(20),
     phone   VARCHAR(10)
 );
 
-CREATE TABLE Order_Item (
+CREATE TABLE OrderItem (
     orderId VARCHAR(20),
     itemId  VARCHAR(20),
-    CONSTRAINT PK         PRIMARY KEY (orderId, itemId),
-    CONSTRAINT FK_orderId FOREIGN KEY (orderId)
-                          REFERENCES MealOrder(orderId),
-    CONSTRAINT FK_itemId  FOREIGN KEY (itemId)
-                          REFERENCES MealItem(itemId)
+    CONSTRAINT PK2        PRIMARY KEY (orderId, itemId),
+    CONSTRAINT FK_orderId2 FOREIGN KEY (orderId)
+                          REFERENCES MealOrder2(orderId),
+    CONSTRAINT FK_itemId2  FOREIGN KEY (itemId)
+                          REFERENCES MealItem2(itemId)
 );
 
